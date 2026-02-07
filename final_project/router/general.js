f6f71ebc91f9c258a0c24fb5e92eef8cd6f40a1b
Angel-L015 <Angel.L015@yahoo.com>
2026-02-07 14:28:00 -0800

    Added axios to call endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios');
 
 // Register a user
 public_users.post("/register", (req, res) => {
@@ -141,6 +141,76 @@ public_users.get('/review/:isbn', function (req, res) {
     return res.status(404).json({ message: error });
   });
 
+});
+
+
+/* 
+
+Task 10–13: 
+Above, these endpoints are implemented with callbacks and Promises. 
+They have now been refactored to use async/await with Axios.
+
+*/
+
+// Helper function to fetch data from requested url
+async function fetchData(url) {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+// Gets list of books (Async/Await with Axios)
+public_users.get('/async', async function(req, res) {
+  try {
+    const bookList = await fetchData('http://localhost:5000/');
+    res.json(bookList);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
+});
+
+// Gets book by isbn (Async/Await with Axios)
+public_users.get('/async/isbn/:isbn', async function(req, res) {
+  try {
+    const requestedIsbn = req.params.isbn;
+    const book = await fetchData('http://localhost:5000/isbn/' + requestedIsbn);
+    res.json(book);
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      res.status(500).json({ message: "Error retrieving book list" });
+    }
+  }
+});
+
+// Gets book by author (Async/Await with Axios)
+public_users.get('/async/author/:author', async function(req, res) {
+  try {
+    const requestedAuthor = req.params.author;
+    const book = await fetchData('http://localhost:5000/author/' + requestedAuthor); // Can return multiple books
+    res.json(book);
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      res.status(500).json({ message: "Error retrieving book list" });
+    }
+  }
+});
+
+// Gets book by title (Async/Await with Axios)
+public_users.get('/async/title/:title', async function(req, res) {
+  try {
+    const requestedTitle = req.params.title;
+    const book = await fetchData('http://localhost:5000/title/' + requestedTitle);
+    res.json(book);
+  } catch (error) {
+    if (error.response) {
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      res.status(500).json({ message: "Error retrieving book list" });
+    }
+  }
 });
 
 module.exports.general = public_users;
